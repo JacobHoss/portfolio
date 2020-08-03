68278cc84f7a59cd80418feaf31de3d9cb2e5ad6
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-08-03 12:43:57 -0400

    more styling changes, added burger + dropdown, added several links

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-// $(function(){
-//     var bgimage = new Image();      
-//     bgimage.src="assets/orange-hue.jpg";       
-//     $(".feature").hide();     
-//     $(bgimage).load(function(){
-//         $(".feature").css("background-image","url("+$(this).attr("src")+")").fadeIn(2000); 
-//      }); 
-// });
-
+	// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+	(function () {
+		var burger = document.querySelector('.burger');
+		var menu = document.querySelector('#' + burger.dataset.target);
+		burger.addEventListener('click', function () {
+			burger.classList.toggle('is-active');
+			menu.classList.toggle('is-active');
+		});
+	})();
