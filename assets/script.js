0a4e08840fa4906a13c82fb62ebc71863787d08d
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-08-02 21:11:27 -0400

    base files and styling

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// $(function(){
+//     var bgimage = new Image();      
+//     bgimage.src="assets/orange-hue.jpg";       
+//     $(".feature").hide();     
+//     $(bgimage).load(function(){
+//         $(".feature").css("background-image","url("+$(this).attr("src")+")").fadeIn(2000); 
+//      }); 
+// });
+
