3363795244588138b7b8809fad242bc68a9fd6dc
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-08-03 20:30:24 -0400

    added much more styling. added links to multiple repos. updated about me summary

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,15 @@
-	// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-	(function () {
-		var burger = document.querySelector('.burger');
-		var menu = document.querySelector('#' + burger.dataset.target);
-		burger.addEventListener('click', function () {
-			burger.classList.toggle('is-active');
-			menu.classList.toggle('is-active');
-		});
-	})();
+// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
+(function () {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#' + burger.dataset.target);
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+function send() {
+    setTimeout(function () {
+        window.open("mailto:" + "jacobhenryhoss2@gmail.com" + "?subject=" + "From " + document.getElementById('name').value + ": " + document.getElementById('subject').value + "&body=" + document.getElementById('message').value);
+    }, 320);
+}
